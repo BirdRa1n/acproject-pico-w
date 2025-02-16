2cb7ae0818e618fb9ab7ad6ab3f6e533214887e3
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2025-02-16 20:24:08 -0300

    definindo cabeçalho inicial

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
+#include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
-
-
+#include <stdio.h>
+#include "lwip/init.h"
+#include "lwip/netif.h"
+#include "pico/binary_info.h"
+#include "src/ssd1306/ssd1306_i2c.h"
+#include "src/ssd1306/ssd1306.h"
+#include "src/display/display.h"
+#include "src/mqtt/mqtt.h"
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
